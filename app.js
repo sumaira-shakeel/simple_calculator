9674f0cbc16f349f4a83e17ec98946fc4d48c733
sumaira-shakeel <160780457+sumaira-shakeel@users.noreply.github.com>
2024-03-23 01:47:51 +0500

    Add files via upload
    A simple calculator using typescript

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let personName = "Eric";
+let message = "would you like to learn some python to day.";
+console.log(`Hello: ${personName} ,${message}`);
---
+++ app.ts
@@ -0,0 +1,9 @@
+let personName :string = "Eric";
+let message : string = "would you like to learn some python to day.";
+console.log(`Hello: ${personName} ,${message}`);
+
+
+
+
+
+
